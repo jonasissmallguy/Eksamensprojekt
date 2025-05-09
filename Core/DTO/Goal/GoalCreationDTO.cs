8fcb9a183c4b0d7821f60475e5a07aa6704e7c4f
ITA504 <rasmuswp2210@gmail.com>
2025-05-09 13:59:21 +0200

    Delmål

---
+++ Core/DTO/Goal/GoalCreationDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.DTO.Goal
+{
+
+    public class GoalCreationDTO
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } // Kursus, delmål...
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Active";
+        public int Semester { get; set; }
+        public int? StarterId { get; set; }
+        public string StarterName { get; set; } = string.Empty;
+        public DateTime DeadLineAt { get; set; }
+
+    }
+
+
+}
---
+++ Core/DTO/Goal/GoalNameDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Core
+{
+    public class GoalNameDTO
+    {
+        public int GoalId { get; set; }
+        public string GoalName { get; set; }
+
+    }
+}
