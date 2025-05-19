8866343e6b956244a1341eb0042366737a15a8e6
Jonas Heidemann <jonasdupontheidemann@gmail.com>
2025-05-19 20:28:40 +0200

    fix

--- Server/Program.cs
+++ Server/Program.cs
@@ -40,11 +40,8 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+app.MapOpenApi();
 
 app.UseRouting();
 
