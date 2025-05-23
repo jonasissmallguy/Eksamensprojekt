933a449b171b893eb35bd127d1dd58af0abd15cd
ITA504 <rasmuswp2210@gmail.com>
2025-05-23 13:34:51 +0200

    Tilføj virker
    Ved ikke hvad jeg har lavet ærligt

--- Core/Model/Elevplan/Goal.cs
+++ Core/Model/Elevplan/Goal.cs
@@ -22,17 +22,17 @@ namespace Core
         
         //Kompetence
         public int? StarterId { get; set; } 
-        public string StarterName { get; set; } = String.Empty;
+        public string? StarterName { get; set; } = String.Empty;
         public DateTime? StartedAt { get; set; } //Starter sætter
         public int? ConfirmerId { get; set; }
-        public string ConfirmerName { get; set; } = String.Empty;
+        public string? ConfirmerName { get; set; } = String.Empty;
         public DateTime? ConfirmedAt { get; set; } //Kok sæltter / Leder
         public DateTime? CompletedAt { get; set; } //Leder sætter
         
         //Skole
         public string? SkoleNavn { get; set; }
         
-        public List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Comment>? Comments { get; set; } = new List<Comment>();
         
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
